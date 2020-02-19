2dc25fa8d1b3bc6068b6fce106556e4f23da2b98
maaarmot <c752961562@163.com>
2020-02-20 00:08:12 +0800

    upload images to UCloud/ufile

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -1,15 +1,33 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.FileDTO;
+import com.example.demo.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 public class FileController {
+
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
     @RequestMapping("/file/upload")
     @ResponseBody
-    public FileDTO upload(){
+    public FileDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartRequest=(MultipartHttpServletRequest)request;
+        MultipartFile file=multipartRequest.getFile("editormd-image-file");
+        try {
+            uCloudProvider.upload(file.getInputStream(),file.getContentType(),file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/images/ad.jpg");
