14f4e8ae6b8f2b9ad90f46e19e32f446d7d14ccb
maaarmot <c752961562@163.com>
2020-02-21 14:31:50 +0800

    repair again

--- src/main/resources/db/migration/V7__Create_comment_table.sql
+++ src/main/resources/db/migration/V7__Create_comment_table.sql
@@ -8,7 +8,3 @@ create table comment
     gmt_modified bigint not null,
     like_count bigint default 0
 );
-comment on column comment.id is '评论id';
-comment on column comment.parent_id is '父类id';
-comment on column comment.type is '类型一级或二级';
-comment on column comment.commentator is '评论人id';
